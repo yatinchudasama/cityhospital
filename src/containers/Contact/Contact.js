1a9f988aed63e40b57b7f86218aaa0b3be86c80a
yatin2123 <yatinv807@gmail.com>
2023-09-27 17:57:56 +0530

    done

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { Heading } from '../../component/UI/Heading/Heading';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
 function Contact(props) {
+    let contactSchema = yup.object().shape({
+        name: yup.string().required("please enter name").matches(/^([a-zA-Z ]){2,30}$/, "plase enter valid name"),
+        email: yup.string().email("please enter valid email").required("please enter email"),
+        subject: yup.string().required("please enter subject").min(10, "minimum 10 worl required"),
+        message: yup.string().required("please enter message")
+    });
+
+
+    const formikobj = useFormik({
+
+        initialValues: {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        },
+
+        validationSchema: contactSchema,
+        onSubmit: values => {
+            console.log(values);
+        },
+
+    })
+
+
+    const { handleChange, handleBlur, handleSubmit, errors,values, touched} = formikobj;
+    console.log(errors);
     return (
         <main>
             <section id="contact" className="contact">
@@ -36,20 +65,63 @@ function Contact(props) {
                             </div>
                         </div>
                         <div className="col-lg-8 mt-5 mt-lg-0">
-                            <form action method="post" role="form" className="php-email-form">
+                            <form onSubmit={handleSubmit} className="php-email-form">
                                 <div className="row">
                                     <div className="col-md-6 form-group">
-                                        <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                                        <input type="text"
+                                            name="name"
+                                            className="form-control"
+                                            id="name"
+                                            placeholder="Your Name"
+                                            values={values.name}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+
+
+                                        />
+                                        {errors.name && touched.name ? <span>{errors.name}</span> : null}
                                     </div>
                                     <div className="col-md-6 form-group mt-3 mt-md-0">
-                                        <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            name="email"
+                                            id="email"
+                                            placeholder="Your Email"
+                                            values={values.email}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+
+                                        />
+                                        {errors.email && touched.email ? <span>{errors.email}</span> : null}
                                     </div>
                                 </div>
                                 <div className="form-group mt-3">
-                                    <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="subject"
+                                        id="subject"
+                                        placeholder="Subject"
+                                        values={values.subject}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+
+                                    />
+                                    {errors.subject && touched.subject ? <span>{errors.subject}</span> : null}
                                 </div>
                                 <div className="form-group mt-3">
-                                    <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
+                                    <textarea
+                                        className="form-control"
+                                        name="message"
+                                        rows={5}
+                                        placeholder="Message"
+                                        values={values.message}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+
+                                    />
+                                    {errors.message && touched.message ? <span>{errors.message}</span> : null}
                                 </div>
                                 <div className="my-3">
                                     <div className="loading">Loading</div>
