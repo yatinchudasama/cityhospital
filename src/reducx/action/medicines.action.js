3749f0f4b9c983babaa0c5a44da57a42c769afe7
patelrutvi <rutvipatel107@gmail.com>
2023-10-17 15:25:45 +0530

    done

--- src/reducx/action/medicines.action.js
+++ src/reducx/action/medicines.action.js
@@ -1,5 +1,5 @@
 import { API_URL } from "../../utils/baseURL";
-import { GET_MEDICINES } from "../ActionType";
+import { DELETE_MEDICINES, GET_MEDICINES } from "../ActionType";
 
 
 export const getmedicines =() => (dispatch) => {
@@ -7,6 +7,18 @@ export const getmedicines =() => (dispatch) => {
         fetch(API_URL + "medicines")
         .then(response => response.json())
         .then(data => dispatch({type: GET_MEDICINES, payload:data}))
+    } catch(error){
+        console.log(error);
+    }
+}
+
+export const deletemedicines =(id) => (dispatch) => {
+    try{
+        fetch(API_URL + "medicines/" + id, {
+            method: "DELETE"
+        })
+      
+        .then(() => dispatch({type: DELETE_MEDICINES, payload:id}))
     } catch(error){
         console.log(error);
     }
